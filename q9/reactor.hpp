cb43ebb7f35e435cf59c9c3fba5cac464973afae
Shira Yogev <54641048+May2303@users.noreply.github.com>
2024-07-19 18:54:20 +0300

    Update reactor.hpp

--- q9/reactor.hpp
+++ q9/reactor.hpp
@@ -9,25 +9,25 @@
 #include <poll.h>
 #include <pthread.h>
 
-typedef std::function<void(int)> reactorFunc;
-typedef void* (*proactorFunc)(int sockfd);
+typedef std::function<void(int)> reactorFunc;        // Define a type for reactor callback functions
+typedef void* (*proactorFunc)(int sockfd);           // Define a type for proactor callback functions
 
 class Reactor {
 public:
-    static Reactor* startReactor();
-    int addFdToReactor(int fd, reactorFunc func);
-    int removeFdFromReactor(int fd);
-    int stopReactor();
-    pthread_t startProactor(int sockfd, proactorFunc threadFunc);
-    int stopProactor(pthread_t tid);
-    void reactorLoop();
+    static Reactor* startReactor();                  // Static method to start the reactor
+    int addFdToReactor(int fd, reactorFunc func);    // Method to add a file descriptor to the reactor
+    int removeFdFromReactor(int fd);                 // Method to remove a file descriptor from the reactor
+    int stopReactor();                               // Method to stop the reactor
+    pthread_t startProactor(int sockfd, proactorFunc threadFunc);  // Method to start a proactor
+    int stopProactor(pthread_t tid);                 // Method to stop a proactor
+    void reactorLoop();                              // Method representing the reactor's main loop
 
 private:
-    Reactor();
-    std::map<int, reactorFunc> fdMap;
-    std::vector<struct pollfd> pollfds;
-    std::mutex mtx;
-    std::atomic<bool> running;
+    Reactor();                                       // Private constructor
+    std::map<int, reactorFunc> fdMap;                // Map to store file descriptors and their callbacks
+    std::vector<struct pollfd> pollfds;              // Vector of poll file descriptors
+    std::mutex mtx;                                  // Mutex to protect shared data
+    std::atomic<bool> running;                       // Atomic flag to indicate if reactor is running
 };
 
 #endif // REACTOR_HPP
