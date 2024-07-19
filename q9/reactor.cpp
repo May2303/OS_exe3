7d4f27fef7364ee8b8cabe95a71e9ccb42c0a246
Shira Yogev <54641048+May2303@users.noreply.github.com>
2024-07-19 18:53:47 +0300

    Update reactor.cpp

--- q9/reactor.cpp
+++ q9/reactor.cpp
@@ -10,59 +10,59 @@ Reactor::Reactor() : running(false) {}
 
 // Start Reactor
 Reactor* Reactor::startReactor() {
-    Reactor* reactor = new Reactor();
-    reactor->running = true;
-    std::thread reactorThread(&Reactor::reactorLoop, reactor);
-    reactorThread.detach();
-    return reactor;
+    Reactor* reactor = new Reactor();            // Create a new Reactor object
+    reactor->running = true;                     // Set the reactor to be running
+    std::thread reactorThread(&Reactor::reactorLoop, reactor);  // Create a new thread for reactor loop
+    reactorThread.detach();                      // Detach thread to allow it to run independently
+    return reactor;                             // Return the reactor object
 }
 
 // Add FD to Reactor
 int Reactor::addFdToReactor(int fd, reactorFunc func) {
-    std::lock_guard<std::mutex> lock(mtx);
-    fdMap[fd] = func;
-    pollfds.push_back({fd, POLLIN, 0});
-    return 0;
+    std::lock_guard<std::mutex> lock(mtx);       // Lock mutex to protect shared data
+    fdMap[fd] = func;                            // Add function callback for given file descriptor
+    pollfds.push_back({fd, POLLIN, 0});          // Add file descriptor to the pollfds list for polling
+    return 0;                                    
 }
 
 // Remove FD from Reactor
 int Reactor::removeFdFromReactor(int fd) {
-    std::lock_guard<std::mutex> lock(mtx);
-    fdMap.erase(fd);
+    std::lock_guard<std::mutex> lock(mtx);       // Lock mutex to protect shared data
+    fdMap.erase(fd);                             // Remove function callback for given file descriptor
     pollfds.erase(std::remove_if(pollfds.begin(), pollfds.end(),
                                  [fd](const struct pollfd& pfd) { return pfd.fd == fd; }),
-                  pollfds.end());
-    return 0;
+                  pollfds.end());                // Remove file descriptor from pollfds list
+    return 0;                                    
 }
 
 // Stop Reactor
 int Reactor::stopReactor() {
-    running = false;
-    return 0;
+    running = false;                             // Set running flag to false to stop reactor loop
+    return 0;                                   
 }
 
 // Reactor Loop
 void Reactor::reactorLoop() {
-    while (running) {
-        std::vector<struct pollfd> pollfdsCopy;
+    while (running) {                            // Loop while reactor is running
+        std::vector<struct pollfd> pollfdsCopy;   // Create a copy of pollfds for thread safety
         {
-            std::lock_guard<std::mutex> lock(mtx);
-            pollfdsCopy = pollfds;
+            std::lock_guard<std::mutex> lock(mtx);  // Lock mutex to protect shared data
+            pollfdsCopy = pollfds;                // Copy pollfds to local variable
         }
-        int ret = poll(pollfdsCopy.data(), pollfdsCopy.size(), 1000);
+        int ret = poll(pollfdsCopy.data(), pollfdsCopy.size(), 1000);  // Poll for events with a timeout of 1000 ms
         if (ret < 0) {
-            perror("poll");
-            break;
+            perror("poll");                      // Print error if poll fails
+            break;                               // Exit loop on poll error
         }
-        if (ret == 0) continue;
-        for (const auto& pfd : pollfdsCopy) {
-            if (pfd.revents & POLLIN) {
-                reactorFunc func;
+        if (ret == 0) continue;                  // Continue loop if no events occurred
+        for (const auto& pfd : pollfdsCopy) {     // Iterate through polled file descriptors
+            if (pfd.revents & POLLIN) {           // Check if input event occurred
+                reactorFunc func;                 // Declare function callback variable
                 {
-                    std::lock_guard<std::mutex> lock(mtx);
-                    func = fdMap[pfd.fd];
+                    std::lock_guard<std::mutex> lock(mtx);  // Lock mutex to protect shared data
+                    func = fdMap[pfd.fd];          // Retrieve function callback for file descriptor
                 }
-                if (func) func(pfd.fd);
+                if (func) func(pfd.fd);           // Call function callback if not null
             }
         }
     }
@@ -70,23 +70,23 @@ void Reactor::reactorLoop() {
 
 // Start Proactor
 pthread_t Reactor::startProactor(int sockfd, proactorFunc threadFunc) {
-    pthread_t tid;
+    pthread_t tid;                               // Declare pthread variable
     if (pthread_create(&tid, nullptr, (void* (*)(void*))threadFunc, (void*)(intptr_t)sockfd) != 0) {
-        perror("pthread_create");
-        exit(EXIT_FAILURE);
+        perror("pthread_create");                // Print error if pthread creation fails
+        exit(EXIT_FAILURE);                      
     }
-    return tid;
+    return tid;                                  // Return pthread ID
 }
 
 // Stop Proactor
 int Reactor::stopProactor(pthread_t tid) {
-    if (pthread_cancel(tid) != 0) {
-        perror("pthread_cancel");
-        return -1;
+    if (pthread_cancel(tid) != 0) {              // Cancel pthread and check for errors
+        perror("pthread_cancel");                // Print error if pthread cancellation fails
+        return -1;                               // Return error
     }
-    if (pthread_join(tid, nullptr) != 0) {
-        perror("pthread_join");
-        return -1;
+    if (pthread_join(tid, nullptr) != 0) {       // Join pthread and check for errors
+        perror("pthread_join");                  // Print error if pthread join fails
+        return -1;                              
     }
-    return 0;
+    return 0;                                    
 }
